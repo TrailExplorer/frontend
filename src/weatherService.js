c22f68e84972ef0caef9bd6c19c8ca14a2185407
PradyumnaChippigiri <prch4538@colorado.edu>
2024-04-13 11:24:02 -0600

    Removed exposure of API key

--- src/weatherService.js
+++ src/weatherService.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-const API_KEY = '4e198bec62b9bef6e1e8bca964e9e6d4';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 export const fetchWeatherData = async (lat, lon) => {
@@ -11,7 +10,7 @@ export const fetchWeatherData = async (lat, lon) => {
         lon: lon,
         exclude: 'minutely,hourly',
         units: 'metric', // Or 'imperial' for Fahrenheit
-        appid: API_KEY,
+        appid: process.env.WEATHER_API_KEY,
       },
     });
     return response.data;
@@ -28,7 +27,7 @@ export const fetchWeatherForecast = async (lat, lon) => {
           lat: lat,
           lon: lon,
           units: 'metric', // Or 'imperial' for Fahrenheit
-          appid: API_KEY,
+          appid: process.env.WEATHER_API_KEY,
         },
       });
       return response.data;
