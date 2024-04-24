7627da91dbbf141462b7c4bbd0c44c229645366b
Govardhan N <Govardhan.Narasimha@colorado.edu>
2024-04-23 23:14:16 -0600

    Requests Patten Changed

--- src/components/Filters/RatingFilte/RatingFilter.jsx
+++ src/components/Filters/RatingFilte/RatingFilter.jsx
@@ -1,33 +1,20 @@
-import { getTrailsByRating } from "../../../requests";
 import TileGroup from "../../TileGroup/TileGroup";
 import "./RatingFilter.css";
-import React, { useEffect, useState } from "react";
 
 const RatingFilter = (props) => {
-    const [selectedRating, setSelectedRating] = useState(null);
-    const [trails, setTrails] = useState([]);
     const ratings = ["3", "3.5", "4", "4.5"];
-    const handleRatingChange = (rating) => {
-        setSelectedRating(rating);
-    };
-
-    useEffect(() => {
-        getTrailsByRating(selectedRating, props?.stateName).then((response) => {
-            setTrails(response);
-        });
-    }, [selectedRating, props?.stateName]);
 
     const popoverContent = (
         <div className="rating-filter">
             {ratings.map((rating) => (
                 <button
                     className={`rating-button ${
-                        selectedRating === rating ? "selected" : ""
+                        props.selectedRating === rating ? "selected" : ""
                     }`}
-                    onClick={() => handleRatingChange(rating)}
+                    onClick={(e) => props.setSelectedRating(e.target.value)}
                     key={rating}
                 >
-                    &gt;= {rating}
+                    &lt;= {rating}
                 </button>
             ))}
         </div>
@@ -39,7 +26,7 @@ const RatingFilter = (props) => {
             className="tile"
             childNo={1}
             popoverContent={popoverContent}
-            trails={trails}
+            trails={props.trails}
         />
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,56 @@
 import React, { useEffect, useState } from "react";
 import "../Home/Home.scss";
 import "../Home/Home.css";
-import Recommendations from "../Filters/Recommendations/Recommendations";
 import RatingFilter from "../Filters/RatingFilte/RatingFilter";
 import LengthFilter from "../Filters/LengthFilter/LengthFilter";
 import DifficultyFilter from "../Filters/DifficultyFilter/DifficultyFilter";
 import { getStateNameByLatitudAndLongitude } from "../../requests";
+import Search from "../Search/Search";
+import states from "./USStates.json";
+import {
+    getTrailsByLength,
+    getTrailsByDifficulty,
+    getTrailsByRating,
+    getSearchResults,
+} from "../../requests";
 
 const Home = () => {
     const [stateName, setStateName] = useState("");
+    const [showSearchResults, setShowSearchResults] = useState(false);
+    const [maxLength, setMaxLength] = useState(1);
+    const [lengthTrails, setLengthTrails] = useState([]);
+    const [selectedDifficulty, setSelectedDifficulty] = useState(7);
+    const [difficultTrails, setDifficultTrails] = useState([]);
+    const [selectedRating, setSelectedRating] = useState(null);
+    const [ratingTrails, setRatingTrails] = useState([]);
+    const [selectedState, setSelectedState] = useState("alabama");
+    const [selectedTrailName, setSelectedTrailName] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            getTrailsByLength(stateName, maxLength).then((response) => {
+                setLengthTrails(response);
+            });
+        }, 500);
+
+        return () => clearTimeout(timer);
+    }, [stateName, maxLength]);
+
+    useEffect(() => {
+        getTrailsByDifficulty(stateName, selectedDifficulty).then(
+            (response) => {
+                setDifficultTrails(response);
+            }
+        );
+    }, [selectedDifficulty, stateName]);
+
+    useEffect(() => {
+        getTrailsByRating(selectedRating, stateName).then((response) => {
+            setRatingTrails(response);
+        });
+    }, [selectedRating, stateName]);
+
     useEffect(() => {
         const getStateName = async () => {
             navigator.geolocation.getCurrentPosition(async (position) => {
@@ -25,6 +67,14 @@ const Home = () => {
         getStateName();
     }, []);
 
+    const handleSearch = (e) => {
+        e.preventDefault();
+        setShowSearchResults(true);
+        getSearchResults(selectedState, selectedTrailName).then((response) => {
+            setSearchResults(response);
+        });
+    };
+
     return (
         <React.Fragment>
             <section className="home">
@@ -38,36 +88,85 @@ const Home = () => {
                             <a href="#home">Explore</a>
                         </button>
                     </div>
-                    <div className="homeCard grid">
+                    <form className="homeCard grid">
                         <div className="inputgroup">
                             <div className="locationDiv">
-                                <label htmlFor="location">Location</label>
-                                <input
-                                    type="text"
-                                    placeholder="Enter Location"
-                                />
+                                <label htmlFor="statename">State Name</label>
+                                <select
+                                    name="stateName"
+                                    required={true}
+                                    value={selectedState}
+                                    onChange={(e) => {
+                                        setSelectedState(e.target.value);
+                                    }}
+                                >
+                                    {states.map((state) => {
+                                        return (
+                                            <option
+                                                key={state}
+                                                value={state.toLowerCase()}
+                                            >
+                                                {state}
+                                            </option>
+                                        );
+                                    })}
+                                </select>
                             </div>
 
-                            <div className="trailLengthDiv">
-                                <label htmlFor="trailLength">
-                                    Trail Length
-                                </label>
+                            <div
+                                className="trailLengthDiv"
+                                value={selectedTrailName}
+                                onChange={(e) => {
+                                    setSelectedTrailName(e.target.value);
+                                }}
+                            >
+                                <label htmlFor="trailName">Trail Name</label>
                                 <input
-                                    type="number"
-                                    className="form-control"
-                                    placeholder="Trail Length"
+                                    type="text"
+                                    name="trailName"
+                                    placeholder="Enter Trail Name"
                                 />
                             </div>
                         </div>
 
-                        <button className="btn">Search</button>
-                    </div>
+                        <button
+                            className="btn"
+                            type="submit"
+                            onClick={handleSearch}
+                        >
+                            Search
+                        </button>
+                    </form>
 
                     <div className="tileGroups grid">
-                        <Recommendations stateName={stateName} />
-                        <RatingFilter stateName={stateName} />
-                        <LengthFilter stateName={stateName} />
-                        <DifficultyFilter stateName={stateName} />
+                        {showSearchResults && (
+                            <Search
+                                title="Search Results"
+                                setShowSearchResults={setShowSearchResults}
+                                trails={searchResults}
+                            />
+                        )}
+                        {!showSearchResults && (
+                            <>
+                                <LengthFilter
+                                    setMaxLength={setMaxLength}
+                                    trails={lengthTrails}
+                                    maxLength={maxLength}
+                                />
+                                <DifficultyFilter
+                                    selectedDifficulty={selectedDifficulty}
+                                    setSelectedDifficulty={
+                                        setSelectedDifficulty
+                                    }
+                                    trails={difficultTrails}
+                                />
+                                <RatingFilter
+                                    selectedRating={selectedRating}
+                                    setSelectedRating={setSelectedRating}
+                                    trails={ratingTrails}
+                                />
+                            </>
+                        )}
                     </div>
                 </div>
             </section>
