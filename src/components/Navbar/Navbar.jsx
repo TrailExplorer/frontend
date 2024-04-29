fbfe9e9e7a8af78a37ac7c111e697792afa0bed0
Govardhan N <ngovardhan566@gmail.com>
2024-04-29 11:24:42 -0600

    Minor Bug Fixes (#21)
    Co-authored-by: Govardhan N <Govardhan.Narasimha@colorado.edu>

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,46 +1,62 @@
-import { useState } from 'react'
-import  styles from './Navbar.module.css';
+import { useState } from "react";
+import styles from "./Navbar.module.css";
 
 function Navbar() {
-    // adding the states 
+    // adding the states
     const [isActive, setIsActive] = useState(false);
     //add the active class
     const toggleActiveClass = () => {
-      setIsActive(!isActive);
+        setIsActive(!isActive);
     };
     //clean up function to remove the active class
     const removeActive = () => {
-      setIsActive(false)
-    }
+        setIsActive(false);
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <nav className={`${styles.navbar}`}>
-            {/* logo */}
-            <a href='/' className={`${styles.logo}`}>TrailExplorer </a>
-            <ul className={`${styles.navMenu} ${isActive ? styles.active : ''}`}>
-              <li onClick={removeActive}>
-                <a href='/' className={`${styles.navLink}`}>Home</a>
-              </li>
-              <li onClick={removeActive}>
-                <a href='/aboutus' className={`${styles.navLink}`}>About Us</a>
-              </li>
-              <li onClick={removeActive}>
-                <a href='#home' className={`${styles.navLink}`}>All products</a>
-              </li>
-              <li onClick={removeActive}>
-                <a href='#home' className={`${styles.navLink}`}>Contact</a>
-              </li>
-            </ul>
-            <div className={`${styles.hamburger} ${isActive ? styles.active : ''}`}  onClick={toggleActiveClass}>
-              <span className={`${styles.bar}`}></span>
-              <span className={`${styles.bar}`}></span>
-              <span className={`${styles.bar}`}></span>
-            </div>
-          </nav>
-        </header>
-      </div>
+        <div className="App">
+            <header className="App-header">
+                <nav className={`${styles.navbar}`}>
+                    {/* logo */}
+                    <a href="/" className={`${styles.logo}`}>
+                        TrailExplorer{" "}
+                    </a>
+                    <ul
+                        className={`${styles.navMenu} ${
+                            isActive ? styles.active : ""
+                        }`}
+                    >
+                        <li onClick={removeActive}>
+                            <a href="/" className={`${styles.navLink}`}>
+                                Home
+                            </a>
+                        </li>
+                        <li onClick={removeActive}>
+                            <a href="/aboutus" className={`${styles.navLink}`}>
+                                About Us
+                            </a>
+                        </li>
+                        <li onClick={removeActive}>
+                            <a
+                                href="https://github.com/TrailExplorer"
+                                className={`${styles.navLink}`}
+                            >
+                                Github
+                            </a>
+                        </li>
+                    </ul>
+                    <div
+                        className={`${styles.hamburger} ${
+                            isActive ? styles.active : ""
+                        }`}
+                        onClick={toggleActiveClass}
+                    >
+                        <span className={`${styles.bar}`}></span>
+                        <span className={`${styles.bar}`}></span>
+                        <span className={`${styles.bar}`}></span>
+                    </div>
+                </nav>
+            </header>
+        </div>
     );
-  }
-  export default Navbar;
-  ;
+}
+export default Navbar;
